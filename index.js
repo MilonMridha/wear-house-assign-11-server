f94579dfdf2ab05639bb37490c4cec9668ff81e2
MilonMridha <milonmridha570@gmail.com>
2022-04-28 15:32:07 -0700

    add first server commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const port = process.env.PORT || 5000;
+const app = express();
+require('dotenv').config()
+
+//middleware 
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4i9gt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+console.log('db connected')
+
+async function run(){
+    try{
+        await client.connect()
+        const perfumeCollection = client.db('house').collection('product');
+
+        app.get('/product', async(req, res)=>{
+            const query = {}
+            const cursor = perfumeCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+    }
+
+    finally{
+
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('My Perfume SERVER IS RUNNING')
+});
+
+app.listen(port, () => {
+    console.log('Perfume  server port is', port)
+});
